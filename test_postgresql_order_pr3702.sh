f4a361b1a88b431868dbb53f2d474108d82b8fed
c-bata <contact@c-bata.link>
2022-06-22 19:11:29 +0900

    check pr 3702

---
+++ test_postgresql_order_pr3702.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "0. Install optuna."
+pip install git+https://github.com/wattlebirdaz/optuna.git@fix-postgres-primarykey-unsorted-problem
+
+echo "1. Prepare PostgreSQL 14.3 Server using Docker."
+docker stop optuna-postgres
+
+set -e
+docker run \
+  -d \
+  --rm \
+  -p 5432:5432 \
+  --platform linux/x86_64 \
+  -e POSTGRES_USER=root \
+  -e POSTGRES_DB=optuna \
+  -e POSTGRES_PASSWORD=root \
+  --name optuna-postgres \
+  postgres:14.3
+
+sleep 20
+
+echo "2. Run the order of trials"
+python example.py
+
+echo "3. Stop PostgreSQL Server"
+docker stop optuna-postgres
